f9bf6865173fc75ce4b97965131d24fb39008377
Tomas <viltetomas2003@gmail.com>
2023-05-22 18:16:56 -0300

    Get the title, release year

--- Ejercicios/firstQuery.sql
+++ Ejercicios/firstQuery.sql
@@ -4,3 +4,7 @@
 -- Ejercicio: Consultar todos los datos de la tabla actor
 
 SELECT * FROM ACTOR
+
+-- Ejercicio: Consultar title, release_year de la tabla film
+
+SELECT title, release_year FROM film
