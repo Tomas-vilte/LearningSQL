4b05eb08165abedb31833ade0234d8789d6af81f
Tomas <viltetomas2003@gmail.com>
2023-05-22 19:25:27 -0300

    Rename columns

--- Ejercicios/firstQuery.sql
+++ Ejercicios/firstQuery.sql
@@ -8,3 +8,7 @@ SELECT * FROM ACTOR
 -- Ejercicio: Consultar title, release_year de la tabla film
 
 SELECT title, release_year FROM film
+
+-- Ejercicio: Cambiar el nombre de title a "Titulo de pelicula" y "release_year" a "Año de lanzamiento" de la tabla film
+
+SELECT title AS "Titulo de pelicula", release_year AS "Año de lanzamiento" FROM film
