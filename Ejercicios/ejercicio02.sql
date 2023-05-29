1362bb69242bb625ae8a0a3cc0dba15a05e842e7
Tomas <viltetomas2003@gmail.com>
2023-05-29 17:13:49 -0300

    Ejercicio02

---
+++ Ejercicios/ejercicio02.sql
@@ -0,0 +1,3 @@
+-- 02. Ejercicio: Asigna un alias a title y escribe "Titulo de la pelicula" y a description escribe "Descripcion de la pelicula" de la tabla film
+
+SELECT title AS "Titulo de la pelicula", description AS "Descripcion de la pelicula" FROM film
