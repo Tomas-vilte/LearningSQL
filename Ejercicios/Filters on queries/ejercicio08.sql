6008df05653d9d0c2535d0fd385f3f4fefe3074b
Tomas <viltetomas2003@gmail.com>
2023-05-31 18:24:25 -0300

    ejercicio08

---
+++ Ejercicios/Filters on queries/ejercicio08.sql
@@ -0,0 +1,8 @@
+-- Consulta utilizando WHERE
+-- WHERE Se utiliza para filtrar registros
+
+-- El operado NOT muestra un registro si la(s) condicion(es) NO ES VERDADERA
+
+-- Ejercicio: Muestra los registros pero que no aparezca Brazil
+
+SELECT * FROM country WHERE country NOT 'brazil'
