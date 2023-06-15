39a48fcb6f42280151c28d005cbc604f400badfc
Tomas <viltetomas2003@gmail.com>
2023-06-15 20:32:47 -0300

    resolucion del ejercicio

---
+++ Ejercicios/Filters on queries/ejercicio24.sql
@@ -0,0 +1,8 @@
+-- Consulta la tabla rental de la base de datos sakila.
+
+-- Filtra la información donde staff_id no sea 1, customer_id sea mayor a 250 y inventory_id sea menor de 100.
+
+SELECT * FROM sakila.rental
+WHERE NOT staff_id = 1
+AND customer_id > 250
+AND inventory_id < 100
