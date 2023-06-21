eae010a39698e58b8014e51813660b3c0ab0d0c6
Tomas <viltetomas2003@gmail.com>
2023-06-21 18:44:59 -0300

    resolucion del ejercicio

---
+++ Ejercicios/Filters on queries/ejercicio27.sql
@@ -0,0 +1,8 @@
+-- Consulta la tabla payment de la base de datos sakila.
+
+-- Filtra la información donde amount esté entre 2.99 y 4.99,  staff_id sea igual a 2 y customer_id sea 1 y 2.
+
+SELECT * FROM sakila.payment 
+WHERE amount BETWEEN 2.99 AND 4.99
+AND staff_id = 2
+AND customer_id IN (1, 2);
