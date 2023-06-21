b27a92b455d5a62b0b2e05a57719141f982d5552
Tomas <viltetomas2003@gmail.com>
2023-06-21 19:21:11 -0300

    resolocion del ejercicio

---
+++ Ejercicios/Filters on queries/ejercicio28.sql
@@ -0,0 +1,6 @@
+-- Consulta la tabla address de la base de datos sakila.
+
+-- Filtra la información donde city_id esté entre 300 y 350,  
+
+SELECT * FROM sakila.address
+WHERE city_id BETWEEN 300 AND 350;
