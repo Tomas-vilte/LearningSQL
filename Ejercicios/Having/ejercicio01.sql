8b9c2776dd4ce3ccd94d2e4f6d9dad23c280146b
Tomas <viltetomas2003@gmail.com>
2023-07-03 20:47:54 -0300

    resolucion del problema

--- Ejercicios/Having/ejercicio01.sql
+++
@@ -1,6 +0,0 @@
--- ¿Qué apellidos no se repiten?
-
-SELECT last_name
-FROM sakila.actor
-GROUP BY last_name
-HAVING COUNT(*) = 1;
---
+++ Ejercicios/INSERT/ejercicio02.sql
@@ -0,0 +1,8 @@
+-- Usa la función INSER INTO y agrega un registro a la tabla address.
+
+INSERT INTO sakila.address
+(address, 
+district,
+postal_code,
+phone)
+VALUES ('123 Main St', 'District 1', '12345', '555-1234')
