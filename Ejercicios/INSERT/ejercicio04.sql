d971bcce1d51dcc9fc4e7cd1e8c296d9fc974cc6
Tomas <viltetomas2003@gmail.com>
2023-07-03 21:04:47 -0300

    resolucion

---
+++ Ejercicios/INSERT/ejercicio04.sql
@@ -0,0 +1,10 @@
+-- Usa la función INSER INTO y agrega un registro a la tabla customer.
+
+INSERT INTO sakila.customer(store_id,
+first_name,
+last_name,
+email)
+VALUES("1",
+"Ali",
+"Vilte",
+"Ali123@sakilacustomer.com");
