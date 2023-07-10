10212de86cc70e8f544935a2e8ba80f0a4881c7e
Tomas <viltetomas2003@gmail.com>
2023-07-10 18:55:24 -0300

    resolucion

--- Ejercicios/ALTER TABLE/ejercicio00.sql
+++ Ejercicios/ALTER TABLE/ejercicio00.sql
@@ -1,13 +1,14 @@
--- Usa la función ALTER TABLE  y agrega la columna categoria a la tabla film_text, llena la columna de los primeros 10 film_id.
+-- Usa la función ALTER TABLE  y agrega la columna categoria a la tabla film_text, llena la columna de los primeros 5 film_id.
 
-ALTER TABLE film_text
+ALTER TABLE sakila.film_text
 ADD categoria VARCHAR(50);
 
-UPDATE film_text
+UPDATE sakila.film_text
 SET categoria = CASE
     WHEN film_id = 1 THEN 'Acción'
     WHEN film_id = 2 THEN 'Comedia'
     WHEN film_id = 3 THEN 'Drama'
-    -- Agrega los valores correspondientes para los primeros 10 film_id aquí
+    WHEN film_id = 3 THEN 'Anime'
+    WHEN film_id = 3 THEN 'Ficcion'
 END
-LIMIT 10;
+LIMIT 5;
